51a6266a92856a5d76cd9c22610decaaa6e32d95
Andrea Piras <braindamage@springlobby.info>
2009-10-24 23:47:25 +0200

    fixed channel autojoin

--- channels.py
+++ channels.py
@@ -3,9 +3,9 @@ from ParseConfig import *
 class Main:
 	sock = 0
 	def onload(self,tasc):
-		pass
+		self.app = tasc.main
 	def oncommandfromserver(self,command,args,socket):
 		pass
 	def onloggedin(self,socket):
-		for channel in self.app.config["channelautojoinlist"]:
+		for channel in parselist(self.app.config["channelautojoinlist"],","):
 			socket.send("JOIN " + channel + "\n")		
