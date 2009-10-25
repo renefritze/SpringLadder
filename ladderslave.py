003c5554792af847e061e274e53d7dd761ec2401
Rene Milk <koshi@springlobby.info>
2009-10-25 21:22:56 +0100

    status parsing

--- ladderslave.py
+++ ladderslave.py
@@ -29,7 +29,34 @@ def saybattle( socket,battleid,message):
 def saybattleex(socket,battleid,message):
 	print green+"Battle:%i, Message: %s" %(battleid,message) + normal
 	socket.send("SAYBATTLEEX %s\n" % message)
-	 	
+
+bstr_nonneg = lambda n: n>0 and bstr_nonneg(n>>1)+str(n&1) or '0'
+
+"""
+    *  b0 = undefined (reserved for future use)
+    * b1 = ready (0=not ready, 1=ready)
+    * b2..b5 = team no. (from 0 to 15. b2 is LSB, b5 is MSB)
+    * b6..b9 = ally team no. (from 0 to 15. b6 is LSB, b9 is MSB)
+    * b10 = mode (0 = spectator, 1 = normal player)
+    * b11..b17 = handicap (7-bit number. Must be in range 0..100). Note: Only host can change handicap values of the players in the battle (with HANDICAP command). These 7 bits are always ignored in this command. They can only be changed using HANDICAP command.
+    * b18..b21 = reserved for future use (with pre 0.71 versions these bits were used for team color index)
+    * b22..b23 = sync status (0 = unknown, 1 = synced, 2 = unsynced)
+    * b24..b27 = side (e.g.: arm, core, tll, ... Side index can be between 0 and 15, inclusive)
+    * b28..b31 = undefined (reserved for future use)
+"""
+
+class BattleStatus:
+	def __init__(self, status, nick ):
+		sstr = bstr_nonneg( int(status) ).rjust( 31, "0" )
+		print 'statusstring length:%d : [%s] from %s'%(len(sstr), sstr,status)
+		self.team = int( sstr[-5:-2], 2)+1
+		self.ally = int( sstr[-9:-6], 2)+1
+		self.side = int( sstr[-27:-24], 2)+1
+		self.nick = nick
+
+	def __str__(self):
+		return "nick: %s -- team:%d ally:%d side:%d"%(self.nick,self.team,self.ally,self.side)
+
 class Main:
 	sock = 0
 	battleowner = ""
@@ -43,6 +70,7 @@ class Main:
 	battleusers = dict()
 	battleoptions = dict()
 	ladderlist = dict()
+	battle_statusmap = dict()
 	battlefounder = ""
 	def gs(self):# Game started
 		self.gamestarted = 1
@@ -243,7 +271,13 @@ class Main:
 				f.write(self.script)
 				f.close()
 				thread.start_new_thread(self.startspring,(s,g))
-			
+		if command == "CLIENTBATTLESTATUS":
+			if len(args) != 3:
+				error( "invalid CLIENTBATTLESTATUS:%s"%(args) )
+			bs = BattleStatus( args[1], args[0] )
+			self.battle_statusmap[ args[0] ] = bs
+			print bs
+		
 	def onloggedin(self,socket):
 		if self.ingame == True:
 			socket.send("MYSTATUS 1\n")
