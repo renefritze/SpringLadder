713b6b4958df6011930716f860706add911d35f1
Rene Milk <koshi@springlobby.info>
2010-02-06 23:13:20 +0100

    three col index

--- website/index.py
+++ website/index.py
@@ -4,7 +4,18 @@
 from jinja2 import Environment, FileSystemLoader
 from ladderdb import *
 from fieldsets import db
+
 env = Environment(loader=FileSystemLoader('templates'))
-template = env.get_template('index.html')
-print template.render(ladders=db.GetLadderList(Ladder.name) )
+
+try:
+	s = db.sessionmaker()
+	limit = 10
+	matches = s.query( Match ).order_by(Match.date.desc())[:limit]
+	matches_header = 'last %i matches'%limit
+	template = env.get_template('index.html')
+	print template.render( matches=matches, matches_header= matches_header  )
+
+except Exception, m:
+	template = env.get_template('error.html')
+	print template.render( err_msg=(str(m)) )
 
