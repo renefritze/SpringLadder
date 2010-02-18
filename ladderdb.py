735f703be96390c29f3ef78e910e00636728dacb
Andrea Piras <braindamage@springlobby.info>
2010-02-18 22:42:41 +0100

    forgot parenthesis

--- ladderdb.py
+++ ladderdb.py
@@ -556,7 +556,7 @@ class LadderDB:
 				for result_table in conflicts:
 					match = session.query( Match ).filter( Match.id == result_table.match_id ).first()
 					laddername = session.query( Ladder ).filter( Ladder.id == match.ladder_id ).first()
-					result += "#%d (%s) ladder: %s (%d)\n" % match.id, match.date, laddername, match.ladder_id
+					result += "#%d (%s) ladder: %s (%d)\n" % (match.id, match.date, laddername, match.ladder_id)
 					#delete the match
 					for r in match.results:
 						session.delete( r )
@@ -570,7 +570,7 @@ class LadderDB:
 				result = "merge failed: conflicts existing, please resolve them manually or use the override switch to automatically delete them; conflicting matches:\n"
 				for match in conflicts:
 					laddername = session.query( Ladder ).filter( Ladder.id == match.ladder_id ).first()
-					result += "#%d (%s) ladder: %s (%d)\n" % match.match_id, match.date, laddername, match.ladder_id
+					result += "#%d (%s) ladder: %s (%d)\n" % (match.match_id, match.date, laddername, match.ladder_id)
 		session.close()
 		if len(conflicts) == 0 or override_conflict: # no conflicts or conflicts autoresolved: recalc ranks and ban the o
 			for ladderid in recalc_ladders: # recalculate ladders which changed
