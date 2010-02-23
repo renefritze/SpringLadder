4a60beab03c7069d61bde721d85af30976c09883
Andrea Piras <braindamage@springlobby.info>
2010-02-23 16:59:30 +0100

    try to change glicko c to 16

--- glicko.py
+++ glicko.py
@@ -8,7 +8,7 @@ class GlickoRankAlgo(IRanking):
 	q = math.log( 10.0 ) / 400.0
 
 	def __init__(self):
-		self.c = 64.0
+		self.c = 16.0
 		self.rd_lower_bound = 50.0
 
 	def Update(self,ladder_id,match,db):
