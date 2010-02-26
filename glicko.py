1412fd169df78ff89db57bf6e2ed59feacc41e7e
Andrea Piras <braindamage@springlobby.info>
2010-02-26 20:54:35 +0100

    re-disabled sorting for best match

--- glicko.py
+++ glicko.py
@@ -148,7 +148,7 @@ class GlickoRankAlgo(IRanking):
 			session.add( playerrank )
 		playerminvalue = playerrank.rating - playerrank.rd
 		playermaxvalue = playerrank.rating - playerrank.rd
-		opponentid = session.query( GlickoRanks ).filter( GlickoRanks.player_id != player_id ).filter( GlickoRanks.ladder_id == ladder_id ).filter( ( (GlickoRanks.rating + GlickoRanks.rd) > playerminvalue and ( GlickoRanks.rating - GlickoRanks.rd ) < playermaxvalue ) or ( playermaxvalue > ( GlickoRanks.rating - GlickoRanks.rd ) and playermaxvalue < (GlickoRanks.rating + GlickoRanks.rd) ) ).order_by( abs(GlickoRanks.rating - playerrank.rating ).asc() )
+		opponentid = session.query( GlickoRanks ).filter( GlickoRanks.player_id != player_id ).filter( GlickoRanks.ladder_id == ladder_id ).filter( ( (GlickoRanks.rating + GlickoRanks.rd) > playerminvalue and ( GlickoRanks.rating - GlickoRanks.rd ) < playermaxvalue ) or ( playermaxvalue > ( GlickoRanks.rating - GlickoRanks.rd ) and playermaxvalue < (GlickoRanks.rating + GlickoRanks.rd) ) )#.order_by( abs(GlickoRanks.rating - playerrank.rating ).asc() )
 		opponents = session.query( Player.nick ).filter( Player.id in opponentid )
 		session.close()
 		return opponents
