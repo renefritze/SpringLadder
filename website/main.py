7ed88695529611b375dff45227246afd202fe211
Rene Milk <koshi@springlobby.info>
2010-02-27 20:11:13 +0100

    on staging allow Roles.User to do admin

--- website/main.py
+++ website/main.py
@@ -12,6 +12,7 @@ config = ParseConfig.readconfigfile( 'Main.conf' )
 Log.Init( 'website.log', 'website.log' )
 db = LadderDB(config['alchemy-uri'])
 env = Environment(loader=FileSystemLoader('templates'))
+staging = 'staging' in config.keys()
 
 @route('/')
 def home():
@@ -41,15 +42,25 @@ def scoreboard_():
 def help_():
 	return help.output( db, env, request )
 
-@route('/admin/ladder', method='POST')
-@AuthDecorator( Roles.Owner, db )
-def admin_ladder():
-	return change_ladder.output( db, env, request )
-@route('/admin/ladder', method='GET')
-@AuthDecorator( Roles.Owner, db )
-def admin_ladder_():
-	return change_ladder.output( db, env, request )
-	
+if staging:
+	@route('/admin/ladder', method='POST')
+	@AuthDecorator( Roles.User, db )
+	def admin_ladder():
+		return change_ladder.output( db, env, request )
+	@route('/admin/ladder', method='GET')
+	@AuthDecorator( Roles.User, db )
+	def admin_ladder_():
+		return change_ladder.output( db, env, request )
+else:
+	@route('/admin/ladder', method='POST')
+	@AuthDecorator( Roles.Owner, db )
+	def admin_ladder():
+		return change_ladder.output( db, env, request )
+	@route('/admin/ladder', method='GET')
+	@AuthDecorator( Roles.Owner, db )
+	def admin_ladder_():
+		return change_ladder.output( db, env, request )
+		
 @route('/fame')
 def fame_():
 	return fame.output( db, env, request )
@@ -63,7 +74,6 @@ def demos(filename):
 	return send_file( filename, root=os.getcwd()+'/demos/' )
 
 port = config['port']
-staging = 'staging' in config.keys()
 debug(staging)
 app = default_app()
 run(app=app,server=PasteServer,host='localhost',port=port , reloader=False)
